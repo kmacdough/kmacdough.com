faaf69a4894697f71c7f67b4b571929a7f8046f7
EC2 Default User <ec2-user@ip-172-31-42-75.us-west-2.compute.internal>
2016-04-08 00:36:19 -0400

    Working basic view for django

--- kmacdough/kmacdough/settings.py
+++ kmacdough/kmacdough/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'testapp.apps.TestappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'kmacdough.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'kmacdoughdb',
+        'USER': 'root',
+        'PASSWORD': 'madbasil',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
